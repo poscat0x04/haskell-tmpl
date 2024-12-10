cbc3a2ba5b96576d016ac9325791b0b4b86cd33a
poscat <poscat@poscat.moe>
2024-12-10 11:13:10 +0800

    format flake.nix using nixfmt

--- flake.nix
+++ flake.nix
@@ -4,30 +4,43 @@
     flake-utils.url = "git+ssh://git@github.com/poscat0x04/flake-utils?shallow=1";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: with flake-utils;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    with flake-utils;
     eachDefaultSystem (
       system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-        in
-          with pkgs;
-          {
-            devShell = {{ project-name }}-dev.envFunc { withHoogle = true; };
-            defaultPackage = {{ project-name }};
-          }
-    ) // {
-      overlay = self: super:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      with pkgs;
+      {
+        devShell = {{ project-name }}-dev.envFunc { withHoogle = true; };
+        defaultPackage = {{ project-name }};
+      }
+    )
+    // {
+      overlay =
+        self: super:
         let
           hpkgs = super.haskellPackages;
-          {{ project-name }} = hpkgs.callCabal2nix "{{ project-name }}" ./. {};
+          {{ project-name }} = hpkgs.callCabal2nix "{{ project-name }}" ./. { };
         in
-          with super; with haskell.lib;
-          {
-            inherit {{ project-name }};
-            {{ project-name }}-dev = addBuildTools {{ project-name }} [
-              haskell-language-server
-              cabal-install
-            ];
-          };
+        with super;
+        with haskell.lib;
+        {
+          inherit {{ project-name }};
+          {{ project-name }}-dev = addBuildTools {{ project-name }} [
+            haskell-language-server
+            cabal-install
+          ];
+        };
     };
 }
